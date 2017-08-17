d2c19107c1bf2a71c37dbb1ea739be5f5b57ce21
pengyuan <532549992@qq.com>
2017-08-17 10:40:45 +0800

    增加调试注释

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ var (
 	kubectlpath    = app.Flag("kubectlpath", "Set kubectl Path").Default("/opt/kubernetes/bin/kubectl").String()
 	kubeconfigpath = app.Flag("kubeconfigpath", "Set kubeconfig Path").Default("/etc/kubernetes/admin.conf").String()
 	projectname    = app.Flag("Projectname", "Set  App  Project  Name. The Jenkins project name is recommended").Default("JOB_NAME").String()
-	namespace      = app.Flag("namespace", "").Default("default").String()
+	namespace      = app.Flag("namespace", `eg: --namespace="default"`).Default("default").String()
 
 	replace = app.Flag("replace", "Set kubernetes pod nubmber").Default("1").Int32()
 	image   = app.Flag("image", "Set docker image name").Default("IMAGE").String()
@@ -25,7 +25,7 @@ var (
 	//containerport = app.Flag("containerport", "Set up the kubernetes internal container port").Default("0").Int32()
 	//cmd  = app.Flag("cmd", "container exec cmd").Default("abc:bcd,aaa:bbb").Strings()
 	//env  = app.Flag("env", "container exec cmd").Default("abc,bcd,aaa,bbb").Strings()
-	port = app.Flag("port", "21:21,22:22").Default("0:0,0:0").String()
+	port = app.Flag("port", `eg: --port="21:21,22:22"`).Default("0:0,0:0").String()
 	http = app.Flag("http", `url 方式发布您的应用 eg：--http="url=http://www.baidu.com,serverport=21,https=false,cafile=$cafilepath,keyfile=$keyfilepath,path=$Path";--http="url=https://www.163.com,serverport=22,https=true,cafile=$cafilepath,keyfile=$keyfilepath,path=$Path"`).Default(`url=http://www.baidu.com,serviceport=0,https=false,cafile=./cafilepath,keyfile=./keyfilepath`, `url=https://www.163.com,servicerport=0,https=true,cafile=./cafilepath1,keyfile=./keyfilepath1`).Strings()
 )
 
@@ -64,6 +64,7 @@ func main() {
 	}
 
 	err = secretcmd(aa.Ingress)
+	fmt.Println(err)
 	if err == nil {
 		for _, v := range sjson {
 			command.Kubectlapply(*kubectlpath, *kubeconfigpath, strconv.Quote(string(v)))
@@ -71,6 +72,7 @@ func main() {
 	}
 
 	err = ingresscmd(aa.Ingress)
+	fmt.Println(err)
 	if err == nil {
 		command.Kubectlapply(*kubectlpath, *kubeconfigpath, strconv.Quote(string(ingjson)))
 
